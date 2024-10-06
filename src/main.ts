af79842b3642cee703240e763f38675370bb6c72
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-10-07 00:53:46 +0300

    adding cookie session

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session')
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['strToEncryptInfo']
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       // The purpose of this property is to make sure
