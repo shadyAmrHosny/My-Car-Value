f7dbe2edd343da268222c97c457880fdbdba9ae8
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-10-14 11:50:36 +0300

    use authGuard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,13 +10,18 @@ import {
    Query,
    UseInterceptors,
    ClassSerializerInterceptor,
-   Session
+   Session,
+  UseGuards,
 } from "@nestjs/common";
+
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./users.service";
 import { SerializeInterceptor, Serialize } from "../interceptors/serialize.interceptor";
 import { UserDto } from "./dtos/user.dto";
 import { AuthService } from "./auth.service";
+import { CurrentUser } from "./decorators/current-user.decorator";
+import { User } from "./user.entity";
+import { AuthGuard } from "../guards/auth.guard";
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
@@ -32,10 +37,17 @@ export class UsersController {
    //    return session.color;
    // }
 
+   // @Get('/whoami')
+   // whoAmI(@Session() session: any){
+   //    return this.usersService.findOne(session.userId);
+   // }
+
+   @UseGuards(AuthGuard)
    @Get('/whoami')
-   whoAmI(@Session() session: any){
-      return this.usersService.findOne(session.userId);
+   whoAmI(@CurrentUser() user: User){
+      return user;
    }
+
    @Post('/signup')
    async createUser(@Body() body:CreateUserDto, @Session() session: any ){
       const user= await this.authService.signup(body.email, body.password);
