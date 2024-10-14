9c1cf9470889b9e9d5451561940b8033be43a795
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-10-14 11:50:17 +0300

    add the user interceptor

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./user.entity";
 import { AuthService } from "./auth.service";
+import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
 
 @Module({
   //this step is what creates repository for us
@@ -11,7 +13,12 @@ import { AuthService } from "./auth.service";
   controllers: [UsersController],
   //A Provider in NestJS is essentially any class or value that can be
   // injected into another class (like a service or controller) using dependency injection.
-  providers: [UsersService, AuthService] // anything that can be injected into other classes
+  providers: [UsersService, AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass:CurrentUserInterceptor
+    },
+  ] // anything that can be injected into other classes
 
 })
 export class UsersModule {}
