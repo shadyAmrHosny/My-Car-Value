973dbec942a0082fa37ad2a392336184b854660e
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-10-25 22:53:21 +0300

    refactor our code to apply the middlewares on the app module to help us in e2e testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,6 +7,7 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users/user.entity";
 import { Report } from "./reports/report.entity";
+import cookieSession from "cookie-session";
 
 @Module({
   //SQLite is a file based database
@@ -19,6 +21,25 @@ import { Report } from "./reports/report.entity";
   }),
     UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_PIPE,
+    useValue: new ValidationPipe({
+      // The purpose of this property is to make sure
+      // that incoming requests don't have extraneous properties
+      //in the body that we are not expecting.
+      whitelist: true
+    })
+  }],
 })
-export class AppModule {}
+
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['strToEncryptInfo'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,21 @@ const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['strToEncryptInfo']
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      // The purpose of this property is to make sure
-      // that incoming requests don't have extraneous properties
-      //in the body that we are not expecting.
-      whitelist: true
-    })
-  )
+  // app.use(
+  //   cookieSession({
+  //   keys: ['strToEncryptInfo']
+  // })
+  // );
+
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     // The purpose of this property is to make sure
+  //     // that incoming requests don't have extraneous properties
+  //     //in the body that we are not expecting.
+  //     whitelist: true
+  //   })
+  // );
+
   await app.listen(3000);
 }
 bootstrap();
