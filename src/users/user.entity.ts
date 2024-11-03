bb7cfbe502991249398196a27bcd3e9bf94ea54b
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-11-04 01:41:18 +0200

    add reports association

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Report } from "../reports/report.entity";
 // import { Exclude } from "class-transformer";
 @Entity()
 export class User {
@@ -11,6 +12,14 @@ export class User {
   @Column()
   // @Exclude()
   password: string;
+
+  @Column({default: true})
+  admin: boolean;
+
+  // the goal of the first arg to tell typeOrm the type of the record user is going to be associated with
+  @OneToMany(()=>Report,(report) => report.user) //--> we have to provide some diff args to the decorator to customize how this behaves
+  reports: Report[]; // add new property that's going to tell typeOrm that we are forming a relationship
+
 
   @AfterInsert()
   logInsert(){
