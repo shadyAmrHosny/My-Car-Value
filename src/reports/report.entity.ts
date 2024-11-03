e07b49c9ec2c26628c6bcfa0ed38e18bf7f230d7
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-11-03 22:43:59 +0200

    Update report.entity.ts

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "../users/user.entity";
 
 @Entity()
 export class Report {
@@ -25,6 +26,13 @@ export class Report {
 
   @Column()
   mileage: number;
+
+  @Column({default: false})
+  approved: boolean;
+
+  @ManyToOne(()=> User,(user) => user.reports)
+  user: User
+
 
 
 
