9a7c62aad8706336aa605097381085959bd791f6
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-11-04 01:46:38 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,46 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from "@nestjs/common";
 import { APP_PIPE } from "@nestjs/core";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users/user.entity";
 import { Report } from "./reports/report.entity";
+import * as process from "process";
 
 const cookieSession = require('cookie-session');
 
 @Module({
   //SQLite is a file based database
   //that is meaning it's going to store all info related to ur DB inside of one single file
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    // TypeOrmModule.forRoot(),
 
-  }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
+    }),
+
+  //   TypeOrmModule.forRoot({
+  //   type: 'sqlite',
+  //   database: 'db.sqlite',     //process.env.NODER_ENV === 'test' ? 'test.sqlite' : 'db.sqlite'
+  //   entities: [User, Report],
+  //   synchronize: true,
+  //
+  // }),
     UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService, {
@@ -33,12 +54,14 @@ const cookieSession = require('cookie-session');
   }],
 })
 
+
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['asdfasfd'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
